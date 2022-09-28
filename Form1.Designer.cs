5e8576dd77d43027fe70dcc66abd49d46edafea0
Aslihan Akbiyik <aslihan.akbiyik@mezun.deu.edu.tr>
2022-09-28 22:19:36 +0300

    added files

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace BasicPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_line = new System.Windows.Forms.Button();
+            this.btn_rect = new System.Windows.Forms.Button();
+            this.btn_ellipse = new System.Windows.Forms.Button();
+            this.btn_eraser = new System.Windows.Forms.Button();
+            this.btn_pencil = new System.Windows.Forms.Button();
+            this.btn_fill = new System.Windows.Forms.Button();
+            this.btn_color = new System.Windows.Forms.Button();
+            this.pic_color = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Sienna;
+            this.panel1.Controls.Add(this.btn_save);
+            this.panel1.Controls.Add(this.btn_clear);
+            this.panel1.Controls.Add(this.btn_line);
+            this.panel1.Controls.Add(this.btn_rect);
+            this.panel1.Controls.Add(this.btn_ellipse);
+            this.panel1.Controls.Add(this.btn_eraser);
+            this.panel1.Controls.Add(this.btn_pencil);
+            this.panel1.Controls.Add(this.btn_fill);
+            this.panel1.Controls.Add(this.btn_color);
+            this.panel1.Controls.Add(this.pic_color);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1974, 126);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.Moccasin;
+            this.btn_save.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_save.ForeColor = System.Drawing.Color.Sienna;
+            this.btn_save.Image = global::BasicPaint.Properties.Resources._2save_buttonJPG;
+            this.btn_save.Location = new System.Drawing.Point(1197, 19);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(100, 100);
+            this.btn_save.TabIndex = 10;
+            this.btn_save.Text = "Save";
+            this.btn_save.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.Color.Moccasin;
+            this.btn_clear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_clear.ForeColor = System.Drawing.Color.Sienna;
+            this.btn_clear.Image = global::BasicPaint.Properties.Resources._2deleteJPG;
+            this.btn_clear.Location = new System.Drawing.Point(1077, 19);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(100, 100);
+            this.btn_clear.TabIndex = 9;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_line
+            // 
+            this.btn_line.BackColor = System.Drawing.Color.Moccasin;
+            this.btn_line.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_line.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_line.ForeColor = System.Drawing.Color.Sienna;
+            this.btn_line.Image = global::BasicPaint.Properties.Resources._2lineJPG;
+            this.btn_line.Location = new System.Drawing.Point(957, 19);
+            this.btn_line.Name = "btn_line";
+            this.btn_line.Size = new System.Drawing.Size(100, 100);
+            this.btn_line.TabIndex = 7;
+            this.btn_line.Text = "Line";
+            this.btn_line.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_line.UseVisualStyleBackColor = false;
+            this.btn_line.Click += new System.EventHandler(this.btn_line_Click);
+            // 
+            // btn_rect
+            // 
+            this.btn_rect.BackColor = System.Drawing.Color.Moccasin;
+            this.btn_rect.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_rect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_rect.ForeColor = System.Drawing.Color.Sienna;
+            this.btn_rect.Image = global::BasicPaint.Properties.Resources._2rectanglejpg;
+            this.btn_rect.Location = new System.Drawing.Point(835, 19);
+            this.btn_rect.Name = "btn_rect";
+            this.btn_rect.Size = new System.Drawing.Size(100, 100);
+            this.btn_rect.TabIndex = 6;
+            this.btn_rect.Text = "Rectangle";
+            this.btn_rect.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_rect.UseVisualStyleBackColor = false;
+            this.btn_rect.Click += new System.EventHandler(this.btn_rect_Click);
+            // 
+            // btn_ellipse
+            // 
+            this.btn_ellipse.BackColor = System.Drawing.Color.Moccasin;
+            this.btn_ellipse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ellipse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ellipse.ForeColor = System.Drawing.Color.Sienna;
+            this.btn_ellipse.Image = global::BasicPaint.Properties.Resources._2ellipse1;
+            this.btn_ellipse.Location = new System.Drawing.Point(714, 19);
+            this.btn_ellipse.Name = "btn_ellipse";
+            this.btn_ellipse.Size = new System.Drawing.Size(100, 100);
+            this.btn_ellipse.TabIndex = 5;
+            this.btn_ellipse.Text = "Ellipse";
+            this.btn_ellipse.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_ellipse.UseVisualStyleBackColor = false;
+            this.btn_ellipse.Click += new System.EventHandler(this.btn_ellipse_Click);
+            // 
+            // btn_eraser
+            // 
+            this.btn_eraser.BackColor = System.Drawing.Color.Moccasin;
+            this.btn_eraser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_eraser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_eraser.ForeColor = System.Drawing.Color.Sienna;
+            this.btn_eraser.Image = global::BasicPaint.Properties.Resources._2eraserJPG;
+            this.btn_eraser.Location = new System.Drawing.Point(593, 19);
+            this.btn_eraser.Name = "btn_eraser";
+            this.btn_eraser.Size = new System.Drawing.Size(100, 100);
+            this.btn_eraser.TabIndex = 4;
+            this.btn_eraser.Text = "Eraser";
+            this.btn_eraser.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_eraser.UseVisualStyleBackColor = false;
+            this.btn_eraser.Click += new System.EventHandler(this.btn_eraser_Click);
+            // 
+            // btn_pencil
+            // 
+            this.btn_pencil.BackColor = System.Drawing.Color.Moccasin;
+            this.btn_pencil.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_pencil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_pencil.ForeColor = System.Drawing.Color.Sienna;
+            this.btn_pencil.Image = global::BasicPaint.Properties.Resources._2pencilJPG;
+            this.btn_pencil.Location = new System.Drawing.Point(472, 19);
+            this.btn_pencil.Name = "btn_pencil";
+            this.btn_pencil.Size = new System.Drawing.Size(100, 100);
+            this.btn_pencil.TabIndex = 3;
+            this.btn_pencil.Text = " Pencil";
+            this.btn_pencil.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_pencil.UseVisualStyleBackColor = false;
+            this.btn_pencil.Click += new System.EventHandler(this.btn_pencil_Click);
+            // 
+            // btn_fill
+            // 
+            this.btn_fill.BackColor = System.Drawing.Color.Moccasin;
+            this.btn_fill.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_fill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_fill.ForeColor = System.Drawing.Color.Sienna;
+            this.btn_fill.Image = global::BasicPaint.Properties.Resources._2fillJPG;
+            this.btn_fill.Location = new System.Drawing.Point(354, 19);
+            this.btn_fill.Name = "btn_fill";
+            this.btn_fill.Size = new System.Drawing.Size(100, 100);
+            this.btn_fill.TabIndex = 2;
+            this.btn_fill.Text = "Fill";
+            this.btn_fill.UseVisualStyleBackColor = false;
+            this.btn_fill.Click += new System.EventHandler(this.btn_fill_Click);
+            // 
+            // btn_color
+            // 
+            this.btn_color.BackColor = System.Drawing.Color.Moccasin;
+            this.btn_color.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_color.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_color.ForeColor = System.Drawing.Color.Sienna;
+            this.btn_color.Image = global::BasicPaint.Properties.Resources.colorJPG1;
+            this.btn_color.Location = new System.Drawing.Point(37, 23);
+            this.btn_color.Name = "btn_color";
+            this.btn_color.Size = new System.Drawing.Size(100, 96);
+            this.btn_color.TabIndex = 1;
+            this.btn_color.Text = "Color";
+            this.btn_color.UseVisualStyleBackColor = false;
+            this.btn_color.Click += new System.EventHandler(this.btn_color_Click);
+            // 
+            // pic_color
+            // 
+            this.pic_color.BackColor = System.Drawing.Color.White;
+            this.pic_color.Location = new System.Drawing.Point(164, 51);
+            this.pic_color.Name = "pic_color";
+            this.pic_color.Size = new System.Drawing.Size(68, 59);
+            this.pic_color.TabIndex = 0;
+            this.pic_color.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 1329);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1974, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.Color.White;
+            this.pic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic.Location = new System.Drawing.Point(0, 0);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(1974, 1429);
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            this.pic.Paint += new System.Windows.Forms.PaintEventHandler(this.pic_Paint);
+            this.pic.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pic_MouseClick);
+            this.pic.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_MouseDown);
+            this.pic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_MouseMove);
+            this.pic.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pic_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1974, 1429);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_fill;
+        private System.Windows.Forms.Button btn_color;
+        private System.Windows.Forms.Button pic_color;
+        private System.Windows.Forms.Button btn_pencil;
+        public System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btn_eraser;
+        private System.Windows.Forms.Button btn_ellipse;
+        private System.Windows.Forms.Button btn_rect;
+        private System.Windows.Forms.Button btn_line;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_save;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Basic Paint
+This app provides write and erase with pencil, draw and fill lines, prepared shapes like squares, rectangles, circles and ellipses 
+for your inner child.
+
+![All in one image](/all-in-one.png)
+![Color Picker](/colors-ss.png)
+![Saved Successfully](/saved.png)
